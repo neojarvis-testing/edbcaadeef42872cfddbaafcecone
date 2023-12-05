b3eea858c058382a82d9136da6182186efa0b19e
Iamneo <techadmin@iamneo.ai>
2023-12-05 09:31:08 +0000

    Submit-project-save-commit-12/5/2023--3:01:05-PM

--- springapp/src/main/java/Demo.java
+++ springapp/src/main/java/Demo.java
@@ -3,6 +3,7 @@ public class Demo {
         System.out.println("welcome");
         System.err.println("qwerty");
         System.out.println("welcome you all");
+        System.out.println("qazxswedcvfr");
     }
     
 }
